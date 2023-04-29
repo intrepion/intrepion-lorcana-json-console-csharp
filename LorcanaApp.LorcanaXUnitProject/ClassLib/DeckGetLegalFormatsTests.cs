9df24ee17879d6ae753eb54421db1a0a279e8e9d
Oliver Forral <intrepion@gmail.com>
2023-04-29 14:46:57 -0600

    red - Deck GetLegalFormats EmptyDeck EmptyList

---
+++ LorcanaApp.LorcanaXUnitProject/ClassLib/DeckGetLegalFormatsTests.cs
@@ -0,0 +1,10 @@
+namespace LorcanaApp.LorcanaXUnitProject.ClassLib;
+
+public class DeckGetLegalFormatsTests
+{
+    [Fact]
+    public void GetLegalFormats_EmptyDeck_EmptyList()
+    {
+        var deck = new Deck();
+    }
+}
