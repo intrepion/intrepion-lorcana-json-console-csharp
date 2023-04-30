7815eb48a1452160d8b654832871893c63f7a1ef
Oliver Forral <intrepion@gmail.com>
2023-04-30 12:38:41 -0600

    red - Card IsLegal EmptyCard False

---
+++ LorcanaApp.LorcanaXUnitProject/ClassLib/CardIsLegalTests.cs
@@ -0,0 +1,10 @@
+namespace LorcanaApp.LorcanaXUnitProject.ClassLib;
+
+public class CardIsLegalTests
+{
+    [Fact]
+    public void IsLegal_EmptyCard_False()
+    {
+        var card = new Card();
+    }
+}
