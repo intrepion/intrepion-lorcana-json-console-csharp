668d27074b26f127aa171161624a03ef7eb8e3dd
Oliver Forral <intrepion@gmail.com>
2023-05-02 20:19:08 -0600

    green - Card.Name

--- LorcanaApp.LorcanaClassLibProject/Card.cs
+++ LorcanaApp.LorcanaClassLibProject/Card.cs
@@ -2,6 +2,8 @@ namespace LorcanaApp.LorcanaClassLibProject;
 
 public class Card
 {
+    public string? Name { get; set; }
+
     public static List<Card> AllCards { get; } = new List<Card>{
         new Card(),
         new Card(),
