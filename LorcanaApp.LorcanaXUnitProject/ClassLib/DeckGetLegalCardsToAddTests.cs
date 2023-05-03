d02e9ebdb0a71398de2b72f7e1ddf9e160d0b4cf
Oliver Forral <intrepion@gmail.com>
2023-05-02 19:57:11 -0600

    red - var cards = deck.GetLegalCardsToAdd(Format.Demo);

--- LorcanaApp.LorcanaXUnitProject/ClassLib/DeckGetLegalCardsToAddTests.cs
+++ LorcanaApp.LorcanaXUnitProject/ClassLib/DeckGetLegalCardsToAddTests.cs
@@ -7,8 +7,12 @@ public class DeckGetLegalCardsToAddTests
     [Fact]
     public void GetLegalCardsToAdd_FormatDemo_AllCards()
     {
+        var expected = 35;
         var deck = new Deck();
 
         var cards = deck.GetLegalCardsToAdd(Format.Demo);
+        var actual = cards.Count;
+
+        Assert.Equal(expected, actual);
     }
 }
