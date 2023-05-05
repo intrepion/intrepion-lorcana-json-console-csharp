b4087e33f7854e99d6a7611591ed7cd01f322a57
Oliver Forral <intrepion@gmail.com>
2023-05-04 22:54:39 -0600

    red - var card = Card.GetCard("ARIEL", "On Human Legs");

---
+++ LorcanaApp.LorcanaXUnitProject/ClassLib/CardGetCardTests.cs
@@ -0,0 +1,11 @@
+namespace LorcanaApp.LorcanaXUnitProject.ClassLib;
+
+public class CardGetCardTests
+{
+    [Fact]
+    public void GetCard_Ariel_Ariel()
+    {
+        var expected = "On Human Legs";
+        var card = Card.GetCard("ARIEL", "On Human Legs");
+    }
+}
