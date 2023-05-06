6da750e9b86d901f125fcdb06971922eb81350ab
Oliver Forral <intrepion@gmail.com>
2023-05-05 22:21:54 -0600

    green - deck.AddCard(card);

--- LorcanaApp.LorcanaClassLibProject/Deck.cs
+++ LorcanaApp.LorcanaClassLibProject/Deck.cs
@@ -2,6 +2,13 @@ namespace LorcanaApp.LorcanaClassLibProject;
 
 public class Deck
 {
+    private List<Card> _cards { get; set; } = new List<Card>();
+
+    public void AddCard(Card card)
+    {
+        _cards.Add(card);
+    }
+
     public List<Card> GetLegalCardsToAdd(Format format)
     {
         return Card.AllCards;
