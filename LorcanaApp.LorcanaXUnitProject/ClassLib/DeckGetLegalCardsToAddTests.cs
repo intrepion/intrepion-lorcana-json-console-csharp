7bcddb11ce714cebf329c86e104c96f62bb300d6
Oliver Forral <intrepion@gmail.com>
2023-05-06 13:44:42 -0600

    red - GetLegalCardsToAdd_FormatDemoCards3Cards3Colors_NoCards

--- LorcanaApp.LorcanaXUnitProject/ClassLib/DeckGetLegalCardsToAddTests.cs
+++ LorcanaApp.LorcanaXUnitProject/ClassLib/DeckGetLegalCardsToAddTests.cs
@@ -39,7 +39,7 @@ public class DeckGetLegalCardsToAddTests
         var expected = 16;
         var deck = new Deck();
         var card1 = Card.GetCard("AURORA", "Briar Rose");
-        var card2 = Card.GetCard("CAPTAIN HOOK", "Forceful Duelist");
+        var card2 = Card.GetCard("BEAST", "Hardheaded");
 
         deck.AddCard(card1);
         deck.AddCard(card1);
@@ -47,6 +47,25 @@ public class DeckGetLegalCardsToAddTests
         deck.AddCard(card2);
         deck.AddCard(card2);
         deck.AddCard(card2);
+
+        var cards = deck.GetLegalCardsToAdd(Format.Demo);
+        var actual = cards.Count;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void GetLegalCardsToAdd_FormatDemoCards3Cards3Colors_NoCards()
+    {
+        var expected = 0;
+        var deck = new Deck();
+        var card1 = Card.GetCard("AURORA", "Briar Rose");
+        var card2 = Card.GetCard("BEAST", "Hardheaded");
+        var card3 = Card.GetCard("CHESHIRE CAT", "Not All There");
+
+        deck.AddCard(card1);
+        deck.AddCard(card2);
+        deck.AddCard(card3);
 
         var cards = deck.GetLegalCardsToAdd(Format.Demo);
         var actual = cards.Count;
