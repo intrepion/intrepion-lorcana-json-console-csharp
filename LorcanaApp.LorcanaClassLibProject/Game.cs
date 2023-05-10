88449aea922f5c93f8e4d1a9bc1f570748000964
Oliver Forral <intrepion@gmail.com>
2023-05-09 18:32:33 -0600

    green - game.GetHistory();

--- LorcanaApp.LorcanaClassLibProject/Game.cs
+++ LorcanaApp.LorcanaClassLibProject/Game.cs
@@ -2,10 +2,12 @@
 
 public class Game
 {
+    public void AddPlayer() { }
+
+    public void GetHistory() { }
+
     public List<int> GetLegalFormats()
     {
         return new List<int>();
     }
-
-    public void AddPlayer() { }
 }
