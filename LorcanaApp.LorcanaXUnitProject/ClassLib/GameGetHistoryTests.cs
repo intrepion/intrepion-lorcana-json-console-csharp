65ca7c3ca05a873f5bfcac22f1ad1c913c913f3b
Oliver Forral <intrepion@gmail.com>
2023-05-09 18:26:15 -0600

    red - var game = new Game();

---
+++ LorcanaApp.LorcanaXUnitProject/ClassLib/GameGetHistoryTests.cs
@@ -0,0 +1,10 @@
+namespace LorcanaApp.LorcanaXUnitProject.ClassLib;
+
+public class GameGetHistoryTests
+{
+    [Fact]
+    public void GetHistory_GameEmpty_ListEmpty()
+    {
+        var game = new Game();
+    }
+}
